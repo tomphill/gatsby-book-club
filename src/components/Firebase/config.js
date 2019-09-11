f0bc628d1a1a0d35e9a952f38550de995dc69fa7
tom <tomphill@live.co.uk>
2019-09-11 19:56:58 +0100

    Add config.

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZkeWjnkFIh0GD-H_jdgpko50T7nOzGFE",
+  authDomain: "gatsby-book-club.firebaseapp.com",
+  databaseURL: "https://gatsby-book-club.firebaseio.com",
+  projectId: "gatsby-book-club",
+  storageBucket: "gatsby-book-club.appspot.com",
+  messagingSenderId: "572519079447",
+  appId: "1:572519079447:web:9146de2183b0ba7c"
+};
+
+export default firebaseConfig;
